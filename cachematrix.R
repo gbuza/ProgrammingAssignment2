3c3fc0ba627cc0343ad6079055d4c3ea54cbaeaf
gbuza <buzagabor1985@gmail.com>
2015-01-24 14:05:03 +0100

    Matrix Cache
    R programming Task for the 3. week.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## author: gbuza
+## Some of my comments are in the script, for testing and practicing.
+## first of all delete the workspace
+rm(list = ls())
+rm(makeCacheMatrix) # or just the function
 
-## Write a short comment describing this function
-
+# makeCacheMatrix <- function(input_data, row, col) {
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  # set <- matrix(data = input_data, nrow = row, ncol = col) {
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+    get <- function() x
+    setmatrix <- function(solve) m <<- NULL
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
+  
+# Test Case, using the function.  
+test <- rbind(c(3, 5),c(-7,2))
+makeCacheMatrix(test)
 
 
-## Write a short comment describing this function
+## These part won't working. The function was compiled, but when i want to test i get an error message:
+## Error in x$getmatrix : $ operator is invalid for atomic vectors
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmatrix(m)
+  m
 }
+# Test Case, using the function.  
+test <- rbind(c(3, 5),c(-7,2))
+makeCacheMatrix(test)
+cacheSolve(test)
+
